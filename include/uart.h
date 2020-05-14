3f69e747e93166f328a58168114efe0587cf0a85
Nicolas Manichon <nmanichon@gmail.com>
2020-05-14 22:34:59 +0200

    uart: Better uart driver

--- include/uart.h
+++ include/uart.h
@@ -5,8 +5,50 @@
 
 #define COM1 0x3F8
 
+#define UART_THR 0
+#define UART_RBR 0
+#define UART_DLL 0
+#define UART_IER 1
+#define UART_DLH 1
+#define UART_IIR 2
+#define UART_FCR 2
+#define UART_LCR 3
+#define UART_MCR 4
+#define UART_LSR 5
+#define UART_MSR 6
+#define UART_SR 7
+
+#define DLAB 0x80
+
+/*
+Base Address	DLAB	I/O Access	Abbrv.	Register Name
+ +0	        0	Write	        THR	Transmitter Holding Buffer
+ +0	        0	Read	        RBR	Receiver Buffer
+ +0	        1	Read/Write	DLL	Divisor Latch Low Byte
+ +1	        0	Read/Write	IER	Interrupt Enable Register
+ +1	        1	Read/Write	DLH	Divisor Latch High Byte
+ +2	        x	Read	        IIR	Interrupt Identification Register
+ +2	        x	Write	        FCR	FIFO Control Register
+ +3	        x	Read/Write	LCR	Line Control Register
+ +4	        x	Read/Write	MCR	Modem Control Register
+ +5	        x	Read	        LSR	Line Status Register
+ +6	        x	Read	        MSR	Modem Status Register
+ +7	        x	Read/Write	SR	Scratch Register
+*/
+
 struct uart_state {
-	int dlab;
+	uint8_t thr;
+	uint8_t rbr;
+	uint8_t dll;
+	uint8_t ier;
+	uint8_t dlh;
+	uint8_t iir;
+	uint8_t fcr;
+	uint8_t lcr;
+	uint8_t mcr;
+	uint8_t lsr;
+	uint8_t msr;
+	uint8_t sr;
 };
 
 int uart_driver(struct uart_state *uart, struct io_request *io, void *data);
--- src/uart.c
+++ src/uart.c
@@ -1,40 +1,106 @@
 #include "uart.h"
 #include "log.h"
+#include "assert.h"
 #include <linux/kvm.h>
 
-static int uart_driver_in(struct uart_state *uart, struct io_request *io,
-			  void *data)
-{
-	(void)uart;
+#define SERIAL_OUT(val)                                                        \
+	do {                                                                   \
+		printf("%c", (val));                                           \
+		fflush(stdout);                                                \
+	} while (0)
 
-	if (io->port == COM1 + 5) {
-		/* we're always "empty" */
-		*(char *)data = 0x20;
+static uint8_t uart_read(struct uart_state *uart, int port)
+{
+	switch (port) {
+	/* case UART_DLL: */
+	case UART_RBR:
+		if (uart->lcr & DLAB)
+			return uart->dll;
+		/* Clear data ready bit */
+		uart->lsr &= ~1;
+		return uart->rbr;
+	/* case UART_IER: */
+	case UART_DLH:
+		if (uart->lcr & DLAB)
+			return uart->dlh;
+		return uart->ier;
+	case UART_IIR:
+		return uart->iir;
+	case UART_LCR:
+		return uart->lcr;
+	case UART_MCR:
+		return uart->mcr;
+	case UART_LSR:
+		return uart->lsr | 0x60;
+	case UART_MSR:
+		return uart->msr;
+	case UART_SR:
+		return uart->sr;
 	}
 
-	return 0;
+	ASSERT(0, "uart_read incorrect port.");
 }
 
-static int uart_driver_out(struct uart_state *uart, struct io_request *io,
-			   void *data)
+static void uart_driver_in(struct uart_state *uart, struct io_request *io,
+			   uint8_t *data)
 {
-	if (io->port == COM1 + 3) {
-		int value = (*(char *)data & 0x80) >> 7;
-		INFO("DLAB=%d\n", value);
-		uart->dlab = value;
+	int port = io->port - COM1;
+	uint8_t ret = uart_read(uart, port);
 
-		return 0;
+	INFO("uart_read(%p, %hhd) = 0x%hhx\n", (void *)uart, port, ret);
+	*data = ret;
+}
+
+static void uart_write(struct uart_state *uart, int port, uint8_t value)
+{
+	switch (port) {
+	/* case UART_THR: */
+	case UART_DLL:
+		if (uart->lcr & DLAB)
+			uart->dll = value;
+		else {
+			uart->thr = value;
+			SERIAL_OUT(value);
+		}
+		break;
+	/* case UART_IER: */
+	case UART_DLH:
+		if (uart->lcr & DLAB)
+			uart->dll = value;
+		uart->ier = value;
+		break;
+	case UART_FCR:
+		uart->fcr = value;
+		break;
+	case UART_LCR:
+		uart->lcr = value;
+		break;
+	case UART_MCR:
+		uart->mcr = value;
+		break;
+	case UART_SR:
+		uart->sr = value;
+		break;
+	default:
+		ASSERT(0, "uart_write incorrect port.");
 	}
+}
 
-	if (io->port == COM1 && !uart->dlab)
-		printf("%c", *(char *)data);
-
-	return 0;
+static void uart_driver_out(struct uart_state *uart, struct io_request *io,
+			    uint8_t *data)
+{
+	int port = io->port - COM1;
+	uint8_t value = *data;
+	INFO("uart_write(%p, %hhd, %hhd)\n", (void *)uart, port, value);
+	uart_write(uart, port, value);
 }
 
 int uart_driver(struct uart_state *uart, struct io_request *io, void *data)
 {
 	if (io->direction == KVM_EXIT_IO_IN)
-		return uart_driver_in(uart, io, data);
-	return uart_driver_out(uart, io, data);
+		uart_driver_in(uart, io, data);
+	else
+		uart_driver_out(uart, io, data);
+
+	return 0;
 }
